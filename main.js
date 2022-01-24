c088d21d56c017af51b013508e2b5ba7f7e10f14
eugene0928 <umid09tuxtayev@gmail.com>
2022-01-25 00:38:58 +0500

    js part is finished

--- main.js
+++ main.js
@@ -18,4 +18,27 @@ function getResult(query) {
 
 function showResult(result) {
     console.log(result)
+    let city = document.querySelector('.location .city')
+    city.innerHTML = `${result.name}  ${result.sys.country}`
+
+    let now = new Date();
+    let date = document.querySelector('.location .date')
+    date.innerHTML = dateBuilder(now)
+
+    let temp = document.querySelector('.main-temp .temp')
+    temp.innerHTML = `${Math.round(result.main.temp)} °C ` 
+
+    let weather = document.querySelector('.main-temp .type-climate');
+    weather.innerHTML = result.weather[0]['main']
+
+    let high_low = document.querySelector('.main-temp .high-low')
+    high_low.innerHTML = `${Math.round(result.main.temp_max)}°C / ${Math.round(result.main.temp_min)}°C`
+
+}
+
+function dateBuilder(info) {
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+    return `${weekdays[info.getDay()]} ${info.getDate()} ${months[info.getMonth()]} ${info.getFullYear()}`
 }
