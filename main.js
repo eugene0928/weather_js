1ff43315cca45d64a6e143b11b68212cfaae2469
eugene0928 <umid09tuxtayev@gmail.com>
2022-01-27 00:35:36 +0500

    navigator module is implemented

--- main.js
+++ main.js
@@ -3,6 +3,136 @@ const api = {
     baseUrl: 'https://api.openweathermap.org/data/2.5/'
 }
 
+function getResult(query) {
+  fetch(`${api.baseUrl}weather?q=${query}&units=metric&APPID=${api.key}`).then(weather => weather.json())
+    .then(showResult)
+}
+
+function showResult(result) {
+  console.log(result)
+  let city = document.querySelector('.location .city')
+  city.innerHTML = `${result.name}`
+
+  let now = new Date();
+  let date = document.querySelector('.location .date')
+  date.innerHTML = dateBuilder(now)
+
+  let temp = document.querySelector('.main-temp .temp')
+  temp.innerHTML = `${Math.round(result.main.temp)} °C `
+
+  let weather = document.querySelector('.main-temp .type-climate');
+  weather.innerHTML = result.weather[0]['main']
+
+
+  switch(result.weather[0]['main']) {
+    case 'Mist':
+      document.body.style.backgroundImage = "url('../src/mist.jpg')";
+      break;
+
+    case 'Haze':
+      document.body.style.backgroundImage = "url('../src/mist.jpg')";
+      break;
+
+    case 'Smoke':
+      document.body.style.backgroundImage = "url('../src/mist.jpg')";
+      break;
+
+    case 'Clouds':
+      document.body.style.backgroundImage = "url('../src/cloudy.jpg')";
+      break;
+
+    case 'Fog':
+      document.body.style.backgroundImage = "url('../src/fog.jpeg')";
+      break;
+
+    case 'Rain':
+      document.body.style.backgroundImage = "url('../src/rainy.jpg')";
+      break;
+
+    case 'Snow':
+      document.body.style.backgroundImage = "url('../src/snowy.jpg')";
+      break;
+
+    case 'Wind':
+      document.body.style.backgroundImage = "url('../src/windy.jpg')";
+      break;
+
+    case 'Clear':
+      document.body.style.backgroundImage = "url('../src/clear.jpg')";
+      break;
+
+    default:
+      document.body.style.backgroundImage = "url('../src/river.jpeg')";
+      break;
+  }
+
+  let high_low = document.querySelector('.main-temp .high-low')
+  high_low.innerHTML = `${Math.round(result.main.temp_max)}°C / ${Math.round(result.main.temp_min)}°C`
+
+}
+
+function dateBuilder(info) {
+  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+  return `${weekdays[info.getDay()]} ${info.getDate()} ${months[info.getMonth()]} ${info.getFullYear()}`
+}
+
+let city = '';
+
+window.addEventListener('load', function() {
+  // Step 1: Get user coordinates
+  function getCoordinates() {
+    let options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    function success(pos) {
+      let crd = pos.coords;
+      let lat = crd.latitude.toString();
+      let lng = crd.longitude.toString();
+      let coordinates = [lat, lng];
+      console.log(`Latitude: ${lat}, Longitude: ${lng}`);
+      getCity(coordinates);
+      return;
+
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }
+
+// Step 2: Get city name
+  function getCity(coordinates) {
+    let xhr = new XMLHttpRequest();
+    let lat = coordinates[0];
+    let lng = coordinates[1];
+
+    // Paste your LocationIQ token below.
+    xhr.open('GET', "https://us1.locationiq.com/v1/reverse.php?key=pk.9c19f9056cc513fa58eef37199d4b406&lat=" +
+      lat + "&lon=" + lng + "&format=json", true);
+    xhr.send();
+    xhr.onreadystatechange = processRequest();
+    xhr.addEventListener("readystatechange", processRequest, false);
+
+    function processRequest(e) {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        let response = JSON.parse(xhr.responseText);
+        city = response.address.city;
+        getResult(city);
+        return city;
+      }
+    }
+  }
+  getCoordinates();
+})
+
+
 const searchBox = document.querySelector('.search-box')
 
 searchBox.addEventListener('keypress', (e) => {
@@ -13,77 +143,3 @@ searchBox.addEventListener('keypress', (e) => {
 })
 
 
-function getResult(query) {
-    fetch(`${api.baseUrl}weather?q=${query}&units=metric&APPID=${api.key}`).then(weather => weather.json())
-        .then(showResult)
-}
-
- function showResult(result) {
-    console.log(result)
-    let city = document.querySelector('.location .city')
-    city.innerHTML = `${result.name}`
-
-    let now = new Date();
-    let date = document.querySelector('.location .date')
-    date.innerHTML = dateBuilder(now)
-
-    let temp = document.querySelector('.main-temp .temp')
-    temp.innerHTML = `${Math.round(result.main.temp)} °C ` 
-
-    let weather = document.querySelector('.main-temp .type-climate');
-    weather.innerHTML = result.weather[0]['main']
-
-   
-    switch(result.weather[0]['main']) {
-        case 'Mist': 
-            document.body.style.backgroundImage = "url('../src/mist.jpg')";
-            break;
-        
-        case 'Haze': 
-            document.body.style.backgroundImage = "url('../src/mist.jpg')";
-            break;
-
-        case 'Smoke': 
-            document.body.style.backgroundImage = "url('../src/mist.jpg')";
-            break;
-        
-        case 'Clouds':
-             document.body.style.backgroundImage = "url('../src/cloudy.jpg')";
-            break;
-
-        case 'Fog':
-            document.body.style.backgroundImage = "url('../src/fog.jpeg')";
-            break;
-
-        case 'Rain':
-            document.body.style.backgroundImage = "url('../src/rainy.jpg')";
-            break;
-
-        case 'Snow':
-            document.body.style.backgroundImage = "url('../src/snowy.jpg')";
-            break;
-
-        case 'Wind':
-            document.body.style.backgroundImage = "url('../src/windy.jpg')";
-            break;
-
-        case 'Clear':
-            document.body.style.backgroundImage = "url('../src/clear.jpg')";
-            break;
-        
-        default:
-            document.body.style.backgroundImage = "url('../src/river.jpeg')";
-            break;
-    }
-
-    let high_low = document.querySelector('.main-temp .high-low')
-    high_low.innerHTML = `${Math.round(result.main.temp_max)}°C / ${Math.round(result.main.temp_min)}°C`
-
-}
-
-function dateBuilder(info) {
-    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-
-    return `${weekdays[info.getDay()]} ${info.getDate()} ${months[info.getMonth()]} ${info.getFullYear()}`
-}
